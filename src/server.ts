4150b88227913f519cc35b71ef6cd85b6d7db0a6
Alexis Sniffer <alexis.sniffer@gmail.com>
2026-02-06 00:28:16 -0500

    instalacion de socket.io

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import next from "next"
+import { createServer } from "node:http"
+import { Server, Socket } from "socket.io"
+
+const dev = process.env.NODE_ENV !== "production"
+const hostname = "localhost"
+const port = 3000
+const app = next({ dev, hostname, port })
+const handler = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler)
+
+  const io = new Server(httpServer)
+
+  io.on("connection", (socket: Socket) => {
+    console.log(`A user connected: ${socket.id}`)
+
+    socket.on("message", (msg: string) => {
+      io.emit("message", msg)
+    })
+
+    socket.on("alert", (msg: { user: string; text: string }) => {
+      io.emit("alert", msg)
+    })
+  })
+
+  io.on("disconnect", (socket: Socket) => {
+    console.log(`A user disconnected: ${socket.id}`)
+  })
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})
