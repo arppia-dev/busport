6700fc282909509d46c884f6b06d9ce13543bdd0
Alexis Sniffer <alexis.sniffer@gmail.com>
2026-02-21 19:23:50 -0500

    modificacion de env del socket

--- src/utils/socketClient.ts
+++ src/utils/socketClient.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client'
 
 export const socket: Socket = io(
-  process.env.WEBSOCKET_URL || 'http://localhost:1337',
+  process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'http://localhost:1337',
   {
     transports: ['websocket'],
     autoConnect: true
